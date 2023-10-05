8caffc0ec9d0e1176ee7108ff2f60819a37c91dd
unknown <ayuverlioni@gmail.com>
2023-10-05 08:17:31 -0700

    Deskripsi singkat tentang perubahan yang Anda lakukan

---
+++ perpustakaan/Perpustakaan.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+// Smart Contract untuk Perpustakaan
+
+pragma solidity ^0.8.0;
+
+contract Perpustakaan {
+    // Struktur data untuk informasi buku
+    struct Buku {
+        string judul;
+        uint tahunDibuat;
+        string penulis;
+    }
+
+    // Penyimpanan data buku berdasarkan ISBN
+    mapping(string => Buku) public bukuDatabase;
+
+    // Daftar admin
+    address public admin;
+
+    // Konstruktor untuk mengatur admin awal
+    constructor() {
+        admin = msg.sender; // Pengirim kontrak pertama kali adalah admin
+    }
+
+    // Modifier untuk membatasi akses hanya untuk admin
+    modifier hanyaAdmin() {
+        require(msg.sender == admin, "Hanya admin yang dapat mengakses");
+        _;
+    }
+
+    // Fungsi untuk menambah buku baru
+    function tambahBuku(string memory judul, uint tahunDibuat, string memory penulis) public hanyaAdmin {
+        string memory isbn = generateUniqueISBN(judul, penulis);
+        bukuDatabase[isbn] = Buku(judul, tahunDibuat, penulis);
+    }
+
+    // Fungsi untuk memperbarui informasi buku
+    function perbaruiBuku(string memory isbn, string memory judul, uint tahunDibuat, string memory penulis) public hanyaAdmin {
+        require(bukuDatabase[isbn].tahunDibuat != 0, "Buku tidak ditemukan");
+        bukuDatabase[isbn] = Buku(judul, tahunDibuat, penulis);
+    }
+
+    // Fungsi untuk menghapus buku
+    function hapusBuku(string memory isbn) public hanyaAdmin {
+        delete bukuDatabase[isbn];
+    }
+
+    // Fungsi untuk mendapatkan informasi buku berdasarkan ISBN
+    function getDataBuku(string memory isbn) public view returns (string memory, uint, string memory) {
+        Buku memory buku = bukuDatabase[isbn];
+        require(buku.tahunDibuat != 0, "Buku tidak ditemukan");
+        return (buku.judul, buku.tahunDibuat, buku.penulis);
+    }
+
+    // Fungsi internal untuk menghasilkan ISBN yang unik
+    function generateUniqueISBN(string memory judul, string memory penulis) internal view returns (string memory) {
+        bytes32 hash = keccak256(abi.encodePacked(judul, penulis, block.timestamp));
+        return string(abi.encodePacked("ISBN-", hash));
+    }
+}
---
+++ perpustakaan/Perpustakaantes.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: UNLICENSED
+//melakukan testing pada perpustakaan.sol
+
+pragma solidity ^0.8.0;
+
+import "./Perpustakaan.sol"; // Impor kontrak utama
+
+contract PerpustakaanTest {
+    Perpustakaan private perpustakaan; // Buat variabel untuk menyimpan instansiasi kontrak utama
+
+    constructor() {
+        perpustakaan = new Perpustakaan(); // Instansiasi kontrak utama
+    }
+
+    // Fungsi untuk menguji tambah buku
+    function testTambahBuku() public {
+        // Persiapkan data buku
+        string memory judul = "Harry Potter";
+        uint tahunDibuat = 1997;
+        string memory penulis = "J.K. Rowling";
+
+        // Panggil fungsi tambah buku dari kontrak utama
+        perpustakaan.tambahBuku(judul, tahunDibuat, penulis);
+
+        // Periksa apakah buku telah ditambahkan dengan benar
+        (string memory judulHasil, uint tahunHasil, string memory penulisHasil) = perpustakaan.getDataBuku("ISBN-123456");
+        assert(keccak256(abi.encodePacked(judulHasil)) == keccak256(abi.encodePacked(judul)));
+        assert(tahunHasil == tahunDibuat);
+        assert(keccak256(abi.encodePacked(penulisHasil)) == keccak256(abi.encodePacked(penulis)));
+    }
+
+    // Fungsi untuk menguji update buku
+function testUpdateBuku() public {
+    // Persiapkan data buku
+    string memory judulBaru = "Harry Potter and the Chamber of Secrets";
+    uint tahunDibuatBaru = 1998;
+    string memory penulisBaru = "J.K. Rowling";
+
+    // Panggil fungsi update buku dari kontrak utama
+    perpustakaan.perbaruiBuku("ISBN-123456", judulBaru, tahunDibuatBaru, penulisBaru);
+
+    // Periksa apakah buku telah diperbarui dengan benar
+    (string memory judulHasil, uint tahunHasil, string memory penulisHasil) = perpustakaan.getDataBuku("ISBN-123456");
+    assert(keccak256(abi.encodePacked(judulHasil)) == keccak256(abi.encodePacked(judulBaru)));
+    assert(tahunHasil == tahunDibuatBaru);
+    assert(keccak256(abi.encodePacked(penulisHasil)) == keccak256(abi.encodePacked(penulisBaru)));
+}
+
+// Fungsi untuk menguji hapus buku
+function testHapusBuku() public {
+    // Panggil fungsi hapus buku dari kontrak utama
+    perpustakaan.hapusBuku("ISBN-123456");
+
+    // Periksa apakah buku telah dihapus dengan benar
+    (string memory judulHasil, uint tahunHasil, string memory penulisHasil) = perpustakaan.getDataBuku("ISBN-123456");
+    assert(keccak256(abi.encodePacked(judulHasil)) == keccak256(abi.encodePacked("")));
+    assert(tahunHasil == 0);
+    assert(keccak256(abi.encodePacked(penulisHasil)) == keccak256(abi.encodePacked("")));
+}
+
+// Fungsi untuk menguji get data buku berdasarkan ISBN
+function testGetDataBuku() public view {
+    // Panggil fungsi get data buku dari kontrak utama
+    (string memory judulHasil, uint tahunHasil, string memory penulisHasil) = perpustakaan.getDataBuku("ISBN-123456");
+
+    // Periksa apakah data buku sesuai dengan yang diharapkan
+    assert(keccak256(abi.encodePacked(judulHasil)) == keccak256(abi.encodePacked("Harry Potter and the Chamber of Secrets")));
+    assert(tahunHasil == 1998);
+    assert(keccak256(abi.encodePacked(penulisHasil)) == keccak256(abi.encodePacked("J.K. Rowling")));
+}
+
+}
